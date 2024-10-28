f8406c6ccbaa31a83da59cbe68c6d2a4de8f7146
sgoo <39872076+sguoo@users.noreply.github.com>
2024-10-28 15:33:49 +0900

    [Bronze V] Title: 구구단, Time: 4 ms, Memory: 856 KB -BaekjoonHub

--- Go/백준/Bronze/2739. 구구단/구구단.go
+++ Go/백준/Bronze/2739. 구구단/구구단.go
@@ -1,1 +1,11 @@
-package main;import "fmt";func main() {var i int;fmt.Scan(&i);for j := 1; j <= 9; j++ {fmt.Println(i, "*", j, "=", i*j)}}
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	fmt.Scan(&i)
+	for j := 1; j <= 9; j++ {
+		fmt.Println(i, "*", j, "=", i*j)
+	}
+}
