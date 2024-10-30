b232cfed553b2bd3f701edce53568a041bbacb8f
sgoo <39872076+sguoo@users.noreply.github.com>
2024-10-30 13:33:06 +0900

    [Bronze V] Title: 새싹, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ C#/백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+
+        }
+    }
+}
