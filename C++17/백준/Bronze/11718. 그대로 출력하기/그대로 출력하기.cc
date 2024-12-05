1a4cee3c88b19b10577a48e9e46951b615cf0259
sgoo <39872076+sguoo@users.noreply.github.com>
2024-12-05 17:24:05 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    string line;
+    while (getline(cin, line)) {
+        cout << line << "\n";
+    }
+    return 0;
+}
